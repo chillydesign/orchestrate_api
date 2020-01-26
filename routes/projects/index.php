ed8d35435d05fca3ffcc2351752b716fba894808
Charles Harvey <harvey.charles@gmail.com>
2020-01-26 16:30:55 +0100

    remove rnadom tasks from projects page

--- routes/projects/index.php
+++ routes/projects/index.php
@@ -10,12 +10,14 @@ $projects = get_projects( array('limit' => $limit, 'offset' => $offset, 'status'
 
 foreach($projects as $project) {
     $project->tasks_count =  tasks_count($project->id);   
-    // if project is active and has incomplete tasks, give it some random tasks to show 
-    if ($project->status == 'active') {
-        if ($project->tasks_count->incomplete > 0 ) {
-            $project->random_tasks =  get_random_incomplete_tasks($project->id, 3);
-        }
-    }
+    // // if project is active and has incomplete tasks, give it some random tasks to show 
+    // if ($project->status == 'active') {
+    //     if ($project->tasks_count->incomplete > 0 ) {
+    //         $project->random_tasks =  get_random_incomplete_tasks($project->id, 3);
+    //     }
+    // }
+
+
 }
     
 
