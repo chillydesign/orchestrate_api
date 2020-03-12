c6968dea8a8a2e32430e97bde830f3bfeba544c1
Charles Harvey <harvey.charles@gmail.com>
2020-03-12 11:49:20 +0100

    add completed at migration

--- migrations.php
+++ migrations.php
@@ -5,6 +5,15 @@ include('connect.php');
 include('functions.php');
 
 
+
+$add_completed_at_to_tasks = "ALTER TABLE `tasks` ADD `completed_at` DATETIME DEFAULT NULL; ";
+if (klxc_add_migration($add_completed_at_to_tasks)) {
+    echo 'added add_completed_at_to_tasks';
+} else {
+    echo 'error add_completed_at_to_tasks';
+};
+
+
 // $add_priority_to_tasks = "ALTER TABLE `tasks` ADD `priority` TINYINT(1) NOT NULL DEFAULT '0' AFTER `indentation`; ";
 // if (klxc_add_migration($add_priority_to_tasks)) {
 //     echo 'added add_priority_to_tasks';
@@ -20,18 +29,18 @@ include('functions.php');
 //     echo 'error add_uploads_table';
 // };
 
-// function klxc_add_migration($query) {
-//     global $conn;
-//         try {
-//             $migration_query = $conn->prepare($query);
-//             $migration_query->execute();
-//             unset($conn);
-//             return true;
+function klxc_add_migration($query) {
+    global $conn;
+        try {
+            $migration_query = $conn->prepare($query);
+            $migration_query->execute();
+            unset($conn);
+            return true;
 
-//         } catch(PDOException $err) {
-//             return false;
+        } catch(PDOException $err) {
+            return false;
 
-//         };
-// }
+        };
+}
 
 ?>
