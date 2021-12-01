2bc3cbb41e4c6f96f97eaa611af370c3e8c3e606
Charles Harvey <harvey.charles@gmail.com>
2021-12-01 17:11:07 +0000

    move cors things to htaccess

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
 ini_set('default_charset', 'UTF-8');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
 header('Content-Type: application/json;charset=UTF-8');
 
 
