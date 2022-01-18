f5afbd9aa2ea29c3e3a6e7d8be3ea2dd21f0b717
Charles Harvey <harvey.charles@gmail.com>
2022-01-18 13:41:49 +0000

    set cors headers in php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 ini_set('default_charset', 'UTF-8');
 header('Content-Type: application/json;charset=UTF-8');
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+
 
 
 include('connect.php');
