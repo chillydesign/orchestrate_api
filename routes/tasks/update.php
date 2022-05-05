7bd70b392ab200ced0977f7b9172ccb7c29764e9
Charles Harvey <harvey.charles@gmail.com>
2022-05-05 09:36:46 +0100

    add check if soeone else edited task before you

--- routes/tasks/update.php
+++ routes/tasks/update.php
@@ -7,33 +7,44 @@ $data = json_decode($json);
 
 if (!empty($data->attributes)) {
 
-
     $task_attributes = $data->attributes;
+    $previous_task = get_task($id);
 
-    if (isset($_GET['single_field'])) {
-        $updated = update_task_field($id, $task_attributes->field, $task_attributes->data);
+    if ($previous_task) {
 
-    } else {
-        $updated = update_task($id, $task_attributes);
 
-    }
 
-    if ($updated) {
-        $task = get_task($id);
-        if ($task) {
-            // change the updated+at date
-            touch_project($task->project_id);
+        if ($previous_task->updated_at == $task_attributes->updated_at) {
+
+
+
+            if (isset($_GET['single_field'])) {
+                $updated = update_task_field($id, $task_attributes->field, $task_attributes->data);
+            } else {
+                $updated = update_task($id, $task_attributes);
+            }
+
+            if ($updated) {
+                $task = get_task($id);
+                if ($task) {
+                    // change the updated+at date
+                    touch_project($task->project_id);
+                }
+                http_response_code(200);
+                echo json_encode($task);
+            } else {
+                http_response_code(404);
+                echo json_encode('Error - task couldnt be updated');
+            }
+        } else {
+            http_response_code(404);
+            echo json_encode('Error - task updated by someone else');
         }
-        http_response_code(200);
-        echo json_encode($task);
     } else {
         http_response_code(404);
-        echo json_encode( 'Error'  );
+        echo json_encode('Error - no previous task');
     }
-
-
-
 } else {
     http_response_code(404);
-    echo json_encode( 'Error'  );
+    echo json_encode('Error - attributes empty');
 }
