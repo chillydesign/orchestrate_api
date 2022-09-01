2ce6489b2b79b3db766a9afb67f173d8d66c1ae1
Charles Harvey <harvey.charles@gmail.com>
2022-09-01 15:25:05 +0100

    sort projects by alphabetical

--- functions_projects.php
+++ functions_projects.php
@@ -89,7 +89,7 @@ function get_projects($opts = null) {
         $cur_sql
         $sear_sql
         $project_id_sql
-        ORDER BY projects.id DESC
+        ORDER BY projects.name ASC
         LIMIT :limit OFFSET :offset ";
 
         // ORDER BY projects.updated_at DESC , projects.status ASC,  projects.incomplete_tasks_count DESC
